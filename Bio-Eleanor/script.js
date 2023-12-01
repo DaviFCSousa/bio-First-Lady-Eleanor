f47cc22bf4628f29ba6f88eaf402b9782c53f5b9
Davi Franco <104522361+DaviFCSousa@users.noreply.github.com>
2023-11-30 21:22:15 -0300

    Add files via upload

---
+++ Bio-Eleanor/script.js
@@ -0,0 +1,5 @@
+function exibirAlerta() {
+    alert("Eleanor Roosevelt was also an active advocate for civil rights and worked tirelessly to promote gender equality and world peace.");
+}
+
+document.getElementById("toggleDetails").addEventListener("click", exibirAlerta);
